b7a5c7bb75915a8ae7acfae2696c103d44eeeb2d
Mark <mark.denyssenko@gmail.com>
2019-05-10 23:31:49 +0300

    added queries

---
+++ BD_lab5/Queies.sql
@@ -0,0 +1,41 @@
+-- ORDERED AVERAGE RATING WITH NAMES
+SELECT 
+	AVG([main].[Rating]) AS [Average rating]
+	,[spectacles].[Title]
+FROM [dbo].[Ratings] AS [main]
+INNER JOIN [dbo].[Spectacles] [spectacles] ON [spectacles].[SpectacleId] = [main].[SpectacleId]
+GROUP BY 
+	[spectacles].[Title]
+ORDER BY [Average rating] DESC
+
+-- MOST RETURNED TICKETS SPECTACLE
+SELECT 
+	[spectacles].Title
+	,COUNT(*) AS [Total Returned Tickets]
+FROM [dbo].[Tickets] [main]
+INNER JOIN [dbo].[Events] [events] ON [events].EventId = [main].EventId
+INNER JOIN [dbo].[Spectacles] [spectacles] ON [spectacles].[SpectacleId] = [events].SpectacleId
+WHERE [main].IsReturned = 1
+GROUP BY 
+	[spectacles].Title
+ORDER BY [Total Returned Tickets] DESC
+
+-- IN WHAT GENRE ACTORS PLAY AT MOST
+
+-- CORRELiruyichi
+-- LAST Spectacles for each actor
+SELECT [main].[ActorId]
+		,[actors].[FirstName]
+		,MAX([lastSpectacle].[Last Spectacle]) AS [Last Event Date]
+FROM [dbo].[ActorsInSpectacles] [main]
+LEFT JOIN [dbo].[Actors] [actors] ON [main].ActorId = [actors].ActorId
+LEFT JOIN (
+	SELECT [events].[SpectacleId], MAX([events].[Date]) AS [Last Spectacle]
+	FROM [dbo].[Events] [events]
+	GROUP BY [events].[SpectacleId]
+) [lastSpectacle] ON [lastSpectacle].[SpectacleId] = [main].[SpectacleId]
+GROUP BY
+	[main].[ActorId],
+	[actors].[FirstName]
+ORDER BY 
+	[main].[ActorId]
